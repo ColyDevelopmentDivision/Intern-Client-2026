ffe4cf60cc7ac2f8a19db0440b1fd0ac784afaba
POG-YOU <chuncaizhenguo0629@gmail.com>
2026-09-12 14:05:17 +0900

    TODO1~3を提出

--- Assets/Scripts/GachaController.cs
+++ Assets/Scripts/GachaController.cs
@@ -162,6 +162,12 @@ namespace GachaWorkshop
 
                 // ───────────────────────────────────────────────
                 // TODO ②：レアが出る直前の「予兆」で“溜め”を作ろう。（書く場所はここ）
+                //───────────────────────────────────────────────
+                if (item.rarity>= Rarity.SR){
+                    int patern = Random.Range(0, 3);
+                    yield return effects.PlayOmen(patern);
+                }
+
                 // TODO ③（TODO②ができたら）：予兆の色をランダムにしよう。
                 //   （TODO②で書いた処理を書き換える）
                 //   問題文は課題シート（Docs/EXERCISES.md）、
@@ -212,8 +218,21 @@ namespace GachaWorkshop
             //   問題文は課題シート（Docs/EXERCISES.md）、
             //   詰まったらヒント集（Docs/HINTS.md）を見てください。
             // ───────────────────────────────────────────────
+            switch(rarity){
+                case Rarity.SSR:
+                    effects.PlaySpecial();
+                    break;
 
-            effects.PlayCommon(); // ← いまは全部これで演出を発生させている状態。まずはここを書き換えよう
+                case Rarity.SR:
+                    effects.PlayRare();
+                    break;
+
+                case Rarity.R:
+                    effects.PlayCommon();
+                    break;
+            }
+
+            //effects.PlayCommon(); // ← いまは全部これで演出を発生させている状態。まずはここを書き換えよう
         }
 
         // ──────────── ここから下は表示まわり（用意済み・編集不要）────────────
